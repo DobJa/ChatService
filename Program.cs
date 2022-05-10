1a619ebccf6784d2d9fb71112b8356b1b0c6b9d6
Dawid Osuchowski <dwdosuchowski@gmail.com>
2022-05-11 01:09:49 +0200

    Add DI for RabbitMQConsumer

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<RabbitMQConsumer>();
+builder.Services.AddSingleton<IMessageConsumer, RabbitMQConsumer>();
 builder.Services.AddSingleton<IMessageProducer, RabbitMQProducer>();
 
 var app = builder.Build();
