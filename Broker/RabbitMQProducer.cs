953e4fbf785b567e369f5168e30aba00473f6690
DobJa <jandobrosolski@gmail.com>
2022-05-10 23:40:48 +0200

    added sneding messages to broker

---
+++ Broker/RabbitMQProducer.cs
@@ -0,0 +1,38 @@
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace ChatService.Broker
+{
+    public class RabbitMQProducer : IMessageProducer
+    {
+        protected readonly ConnectionFactory _factory;
+        protected readonly IConnection _connection;
+        protected readonly IModel _channel;
+
+        protected readonly IServiceProvider _serviceProvider;
+
+        public RabbitMQProducer(IServiceProvider serviceProvider)
+        {
+            _factory = new ConnectionFactory() { HostName = "rabbitmq" };
+            _connection = _factory.CreateConnection();
+            _channel = _connection.CreateModel();
+
+            System.Console.WriteLine("\nConnected to poop factory!\n");
+
+            _serviceProvider = serviceProvider;
+        }
+
+        public void SendMessage<T>(T message)
+        {
+            System.Console.WriteLine("\nConnected to poop factory!\n");
+
+            _channel.QueueDeclare(queue: "chat2", durable: false, exclusive: false, autoDelete: false);
+
+            var json = JsonConvert.SerializeObject(message);
+            var body = Encoding.UTF8.GetBytes(json);
+
+            _channel.BasicPublish(exchange: "", routingKey: "chat", body: body);
+        }
+    }
+}
