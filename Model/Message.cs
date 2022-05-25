c46f8acc726a5ae845ebd5cc594eec03f49969b6
Dawid Osuchowski <dwdosuchowski@gmail.com>
2022-05-25 22:28:37 +0200

    change MessId to string

--- Model/Message.cs
+++ Model/Message.cs
@@ -2,7 +2,7 @@ namespace ChatService.Model
 {
     public class Message
     {
-        public UInt64 MessId { get; set; }
+        public String MessId { get; set; }
         public String User { get; set; }
         public String Text { get; set; }
         public DateTime Timestamp { get; set; }
